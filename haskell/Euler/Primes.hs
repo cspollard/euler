13b49193a11c0ed724384a1a27b41a8eadec73b7
Chris <cspollard@gmail.com>
2014-05-26 19:01:29 -0400

    about to change prime strategy.

--- haskell/Euler/Primes.hs
+++ haskell/Euler/Primes.hs
@@ -1,8 +1,6 @@
 module Euler.Primes where
 
-import Data.MemoTrie
 import Data.Function (fix)
-import Control.Arrow
 
 sieve :: Integral a => [a] -> [a]
 sieve [] = []
@@ -18,22 +16,7 @@ primeHelper _ 0 = []
 primeHelper _ 1 = []
 primeHelper f x = p : f (x `div` p)
     where
-        p = head . dropWhile (\y -> (rem x y /= 0)) $ primes
+        p = head . dropWhile (\y -> rem x y /= 0) $ primes
 
-
--- currently not working: need a way to not start over at [2..] every
--- time. may have to roll back commit.
-primeHelper' :: Integral a => (a -> [a] -> [a]) -> a -> [a] -> [a]
-primeHelper' _ 0 _ = []
-primeHelper' _ 1 _ = []
-primeHelper' f x ps = p : f (x `div` p) ps'
-    where
-        ps' = dropWhile (\y -> (rem x y /= 0)) ps
-        p = head ps'
-
-
-primeFactors :: (HasTrie a, Integral a) => a -> [a]
-primeFactors = memo (primeHelper primeFactors)
-
-primeFactors' :: Integral a => a -> [a]
-primeFactors' = fix primeHelper
+primeFactors :: Integral a => a -> [a]
+primeFactors = fix primeHelper
