13d99fd45e1ab6e1349ce7b18c1dc79683a01d7c
Chris <cspollard@gmail.com>
2014-05-26 16:56:56 -0400

    moving from mod to rem since it's faster.

--- haskell/Euler/Divisor.hs
+++ haskell/Euler/Divisor.hs
@@ -1,16 +1,16 @@
 module Euler.Divisor where
 
 import Prelude hiding (lcm, gcd)
-import Euler.Primes (primeFactors)
-import Data.List (subsequences, nub)
+import qualified Data.MemoTrie as MT
+
 
 isDivisorOf :: Integral a => a -> a -> Bool
-isDivisorOf n m = mod m n == 0
+isDivisorOf n m = rem m n == 0
 
 divis' :: Integral a => a -> a -> a -> [a]
 divis' x cur end = if cur >= end
                         then []
-                        else if x `mod` cur == 0
+                        else if x `rem` cur == 0
                                 then let y = x `div` cur in cur : y : divis' x (cur+1) (y-1)
                                 else divis' x (cur+1) end
 
@@ -18,10 +18,23 @@ allDivisors :: Integral a => a -> [a]
 allDivisors x = divis' x 1 x
 -- allDivisors a = nub . map product . subsequences . primeFactors $ a
 
+
+{-
+divisorsHelp :: Integral a => (a -> Set a) -> a -> Set a
+divisorsHelp _ 0 = S.empty
+divisorsHelp _ 1 = S.singleton 1
+divisorsHelp accessor x = accessor (x `div` p) `S.insert` x `S.insert` (x `div` p)
+    where p = head . dropWhile ((/=0) . rem x) $ primes
+
+
+divisors' :: Integral a => (a -> [a])
+divisors' = MT.memo
+-}
+
 gcd :: Integral a => a -> a -> a
 gcd x 0 = x
 gcd x y = if x > y
-            then gcd y (mod x y)
+            then gcd y (rem x y)
             else gcd y x
 
 lcm :: Integral a => a -> a -> a
--- haskell/Euler/Primes.hs
+++ haskell/Euler/Primes.hs
@@ -1,11 +1,12 @@
 module Euler.Primes where
 
-import Euler.IntTree
 import Data.MemoTrie
+import Data.Function (fix)
+import Control.Arrow
 
 sieve :: Integral a => [a] -> [a]
 sieve [] = []
-sieve (p:ps) = p : sieve (filter ((/= 0) . (`mod` p)) ps)
+sieve (p:ps) = p : sieve (filter ((/= 0) . (`rem` p)) ps)
 
 primes :: Integral a => [a]
 primes = sieve [2..]
@@ -13,8 +14,26 @@ primes = sieve [2..]
 -- prime factors
 
 primeHelper :: Integral a => (a -> [a]) -> a -> [a]
+primeHelper _ 0 = []
+primeHelper _ 1 = []
 primeHelper f x = p : f (x `div` p)
-    where p = head . dropWhile ((/=0) . mod x) $ primes
+    where
+        p = head . dropWhile (\y -> (rem x y /= 0)) $ primes
+
+
+-- currently not working: need a way to not start over at [2..] every
+-- time. may have to roll back commit.
+primeHelper' :: Integral a => (a -> [a] -> [a]) -> a -> [a] -> [a]
+primeHelper' _ 0 _ = []
+primeHelper' _ 1 _ = []
+primeHelper' f x ps = p : f (x `div` p) ps'
+    where
+        ps' = dropWhile (\y -> (rem x y /= 0)) ps
+        p = head ps'
+
 
 primeFactors :: (HasTrie a, Integral a) => a -> [a]
 primeFactors = memo (primeHelper primeFactors)
+
+primeFactors' :: Integral a => a -> [a]
+primeFactors' = fix primeHelper
