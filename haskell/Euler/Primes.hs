9306276e6ff8fa0bd8fa10a27417f6247be7550e
Chris <cspollard@gmail.com>
2014-05-27 16:56:46 -0400

    much faster primes generation.

--- haskell/Euler/Primes.hs
+++ haskell/Euler/Primes.hs
@@ -1,22 +1,26 @@
 module Euler.Primes where
 
-import Data.Function (fix)
-
-sieve :: Integral a => [a] -> [a]
-sieve [] = []
-sieve (p:ps) = p : sieve (filter ((/= 0) . (`rem` p)) ps)
+import Data.List.Ordered (minus)
 
 primes :: Integral a => [a]
-primes = sieve [2..]
+primes = sieve [2..] 4 primes
+    where
+        sieve :: Integral a => [a] -> a -> [a] -> [a]
+        sieve [] _ _ = []
+        sieve (x:xs) q pps@ ~(p:ps)
+            | x < q = x : sieve xs q pps
+            | otherwise = sieve (xs `minus` map (*p) [p+1..]) (head ps ^ 2) ps
 
 -- prime factors
 
-primeHelper :: Integral a => (a -> [a]) -> a -> [a]
-primeHelper _ 0 = []
-primeHelper _ 1 = []
-primeHelper f x = p : f (x `div` p)
-    where
-        p = head . dropWhile (\y -> rem x y /= 0) $ primes
-
 primeFactors :: Integral a => a -> [a]
-primeFactors = fix primeHelper
+primeFactors = primeFactors' primes
+
+primeFactors' :: Integral a => [a] -> a -> [a]
+primeFactors' _ 0 = []
+primeFactors' _ 1 = []
+primeFactors' [] n = [n]
+primeFactors' (p:ps) n
+    | p*p > n = [n]
+    | n `rem` p == 0 = p : primeFactors' (p:ps) (n `div` p)
+    | otherwise = primeFactors' ps n
