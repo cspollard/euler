c5b2846d5928f8581701e11ee9db783614eb605f
Chris Pollard <cspollard@gmail.com>
2016-01-27 15:48:16 +0000

    fixing e001.

---
+++ haskell/e001.hs
@@ -0,0 +1,6 @@
+import Control.Applicative (liftA2)
+
+divisibleBy :: Integral a => a -> a -> Bool
+x `divisibleBy` y = (x `mod` y) == 0
+
+e001 = sum . filter (liftA2 (||) (`divisibleBy` 3) (`divisibleBy` 5)) $ [1..999]
--- haskell/e1.hs
+++
@@ -1,3 +0,0 @@
-main :: IO ()
-main = do
-    print $ sum [x | x <- [1..999], ((x `mod` 5) == 0) || ((x `mod` 3) == 0)]
